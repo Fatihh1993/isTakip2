f2519d3c05bbd992ce05e0c703e13dda55fc1a66
Fatihh1993 <82492334+Fatihh1993@users.noreply.github.com>
2021-06-07 00:34:31 +0300

    new

---
+++ UdemyIdentity/UdemyIdentity/Context/UdemyContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UdemyIdentity.Context
+{
+    public class UdemyContext : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=FATIH\\SQLEXPRESS;database=UdemyIdentity; integrated security=true; ");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
